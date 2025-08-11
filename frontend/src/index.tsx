e312d293d0f26105c1b9c39a1002996a843dfa60
felixkrutov <googoldecaplex@bk.ru>
2025-08-11 16:21:27 +0300

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,7 @@
-// frontend/src/index.tsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import './index.css'; 
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
