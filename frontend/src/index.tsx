3a8a8d658c8f33c499f8823a3ee25693cf119980
felixkrutov <googoldecaplex@bk.ru>
2025-08-11 15:59:55 +0300

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,9 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './context/AuthContext'; // ADDED: Импортируем наш провайдер
-
-// Стили лучше импортировать здесь, чтобы они были доступны всему приложению
+import { AuthProvider } from './context/AuthContext';
 import './index.css'; 
 
 const rootElement = document.getElementById('root') as HTMLElement;
@@ -13,7 +11,6 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    {/* ADDED: Оборачиваем App в AuthProvider */}
     <AuthProvider>
       <App />
     </AuthProvider>
